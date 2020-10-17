facb29077e1b0019861b523050a7a0b626b57276
nine-eleven <thedevilwearspradaoops@gmail.com>
2020-10-17 19:29:24 +0300

    feat: completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
   
+n= String (n);
+let result = "";
+
+if (n.includes("-")) { n = n.slice(1)} ;
+
+for (let i = (n.length - 1); i >= 0 ; i -= 1) {
+    result += n[i];
+}
+
+return +result;
+
 }
